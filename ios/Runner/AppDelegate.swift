7e1453f80428ba4ddfeaabc6035672256acf26b6
FateAhmed <ahmad.arshad19@yahoo.com>
2025-12-24 12:43:23 +0500

    Configure iOS for Firebase push notifications
    - Add GoogleService-Info.plist for Firebase configuration
    - Update AppDelegate with Firebase initialization and messaging delegate
    - Add background modes for remote notifications
    - Configure Podfile for static framework linkage
    - Update pod dependencies

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 import GoogleMaps
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +10,39 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Firebase
+    FirebaseApp.configure()
+
+    // Google Maps
     GMSServices.provideAPIKey("AIzaSyBLTm_mUtLfjWxUZD5YB4_BNoYXz-AUw5U")
+
+    // Register for remote notifications
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
+    } else {
+      let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+    application.registerForRemoteNotifications()
+
+    // Set Messaging delegate
+    Messaging.messaging().delegate = self
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ application: UIApplication,
+                           didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    // Token will be handled by Flutter side
+    print("FCM Token: \(fcmToken ?? "")")
   }
 }
